27d368d1a78e4e8e7b39e455360bdb821e9ed471
iyboklee <iyboklee@gmail.com>
2019-04-08 00:33:37 +0900

    graphql 에러 응답 메시지 처리

--- src/main/kotlin/com/github/iyboklee/serving/GraphQL.kt
+++ src/main/kotlin/com/github/iyboklee/serving/GraphQL.kt
@@ -1,16 +1,22 @@
 package com.github.iyboklee.serving
 
 import com.github.iyboklee.server.ApiServerContext
+import com.github.pgutkowski.kgraphql.RequestException
 import com.github.pgutkowski.kgraphql.schema.Schema
 import io.ktor.application.call
 import io.ktor.http.ContentType
 import io.ktor.request.receive
+import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.Routing
 import io.ktor.routing.post
 import io.ktor.routing.route
 
 data class GraphQLRequest(val query: String)
+
+data class Error(val message: String?, val errorType: String)
+
+data class Errors(val errors: List<Error>)
 
 fun Routing.graphQL(context: ApiServerContext, prefix: String) {
     execute(prefix) {
@@ -22,8 +28,14 @@ private fun Routing.execute(prefix: String, getSchema: () -> Schema) {
     route(prefix) {
         post {
             val request = call.receive<GraphQLRequest>()
-            val response = getSchema().execute(request.query)
-            call.respondText(response, ContentType.Application.Json)
+            try {
+                val response = getSchema().execute(request.query)
+                call.respondText(response, ContentType.Application.Json)
+            } catch (e: RequestException) {
+                call.respond(Errors(listOf(
+                    Error(e.message, e.javaClass.simpleName)
+                )))
+            }
         }
     }
 }
